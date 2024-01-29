16aca518c1ae299a32b84dbbecc6f13e476a251f
Ramin RX7 <rawmin.rx@gmail.com>
2024-01-29 18:02:45 +0330

    feat: add urls for film search and critique search

--- film/urls.py
+++ film/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('film_list/', views.FilmList.as_view(), name='film_list'),
     path('like_film/', views.LikeFilm.as_view(), name='like_film'),
     path('score_film/', views.ScoreFilm.as_view(), name='score_film'),
-
+    path('search/', views.SearchFilmView.as_view(), name='search'),
+    path("critique-search/", views.SearchCritiqueView.as_view(), name="critique_search")
 ]
