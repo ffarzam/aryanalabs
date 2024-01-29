aa795c6f2f112c31972bfb87dd9e15a2bf98ec36
Ramin RX7 <rawmin.rx@gmail.com>
2024-01-29 18:02:26 +0330

    feat: add critique search view

--- film/views.py
+++ film/views.py
@@ -105,3 +105,66 @@ class SearchFilmView(APIView):
             raise e
             return JsonResponse({'error': str(e)}, status=500)
 
+
+class SearchCritiqueView(APIView):
+    def get(self, request, *args, **kwargs):
+        date_range_query = {}
+        if end_time:=request.GET.get("date_lte"):
+            end_time = datetime.strptime(end_time, "%Y-%m-%d")
+            date_range_query["lte"] = end_time.isoformat()
+        if start_time:=request.GET.get("date_gte"):
+            start_time = datetime.strptime(start_time, "%Y-%m-%d")
+            date_range_query["gte"] = start_time.isoformat()
+        query_dict = {
+            "query": {
+                "nested": {
+                    "path": "critique_set",
+                    "query": {
+                        "bool": {
+                            "must": [
+                                {
+                                "query_string": {
+                                    "default_field": "critique_set.content",
+                                    "query": f'"{request.GET.get("content")}"'
+                                }
+                                },
+                                {
+                                    "query_string": {
+                                        "default_field": "critique_set.is_sploiler",
+                                        "query": f"{request.GET.get('is_sploiler')}"
+                                    }
+                                },
+                                {
+                                    "range": {
+                                        "critique_set.date_submitted": {
+                                            **date_range_query
+                                        }
+                                    }
+                                }
+                            ]
+                        },
+                    },
+                    "inner_hits": {
+                        "name": "matching_critiques",
+                        "highlight": {
+                            "fields": {
+                            "critique_set.content": {}
+                            }
+                        }
+                    }
+                },
+            },
+        }
+
+        result = ES_CONNECTION.search(index=index_name, body=query_dict)
+        out = result["hits"]["hits"]
+
+        # film_data = {**(out[0]), **}
+
+        return JsonResponse({"data":out}, status=200)
+
+        # serializer = FilmWithCritiquesSerializer(data=out[0])
+        # serializer.is_valid(raise_exception=True)
+        # return Response(serializer.data)
+# amirabbas@arianalabs.io
+# mohammadreza@
